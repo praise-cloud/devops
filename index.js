dac05d51f9af86117f25f3443471acf28a6cb1c9
praise-cloud <georgewillpraise1@gmail.com>
2025-09-18 16:24:07 +0100

    add pipelines

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("HGello devops")
+console.log("i am learning ci/cd with github action")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting Test ...");
+setTimeout(() => {
+  console.log("waiting 3 seconds ...");
+}, 3000);
+console.log("Test Completed!");
